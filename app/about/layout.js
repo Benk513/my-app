0b5443b353c00c8370bc9cd887cfd9359c27dd0a
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-06-25 22:10:56 +0100

    This is my very first commit , i am learning Next Js

---
+++ app/about/layout.js
@@ -0,0 +1,11 @@
+export default function RootLayout({ children, } ) {
+    return (
+        <>
+            <nav>About NavBar</nav>
+            <main>{children}</main>
+
+        </>
+       
+        
+    );
+  }
---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <>
+
+      <h1>about</h1>
+      <Link href="/">Link to Home Page</Link>
+    </>
+
+  )
+}
+
